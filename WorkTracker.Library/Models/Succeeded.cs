4b679d330193869f4643be2359d7cd4a35a38e8f
Adam ONeil <adamosoftware@gmail.com>
2020-03-21 12:41:19 -0400

    Add project files.

---
+++ WorkTracker.Library/Models/Succeeded.cs
@@ -0,0 +1,24 @@
+﻿using AO.DbSchema.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkTracker.Library.Models
+{
+    [Schema(WorkTracker.Schema)]
+    public class Succeeded
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string Queue { get; set; }
+
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string Data { get; set; }
+
+        public DateTime Started { get; set; }
+
+        public DateTime Finished { get; set; } = DateTime.UtcNow;
+    }
+}
