8b092e099ed3e3804805bab3543b7ba436f2184e
Adam ONeil <adamosoftware@gmail.com>
2020-04-04 09:05:45 -0400

    some test improvements

--- JobManager.Test/BasicTests.cs
+++ JobManager.Test/BasicTests.cs
@@ -1,33 +1,71 @@
 using JobManager.Library;
+using JobManager.Library.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlServer.LocalDb;
 using System;
+using Dapper.CX.Extensions;
+using Dapper.CX.SqlServer.Extensions.Long;
 
 namespace JobManager.Test
 {
     [TestClass]
     public class BasicTests
     {
+        private static SqlConnection GetConnection() => LocalDb.GetConnection("JobTracker");
+
         [TestMethod]
         public void SimpleSucceedJob()
-        {            
-            Func<SqlConnection> getConnection = () => LocalDb.GetConnection("JobTracker");
+        {
+            long jobId = 0;
 
-            using (var job = JobTracker.StartAsync("adamo", getConnection).Result)
+            using (var job = JobTracker.StartAsync("adamo", GetConnection).Result)
             {
                 // doesn't matter what's in here
+                jobId = job.JobId;
             }
+
+            AssertJobExists(jobId);
         }
 
         [TestMethod]
         public void SimpleFailedJob()
         {
-            Func<SqlConnection> getConnection = () => LocalDb.GetConnection("JobTracker");
+            long jobId = 0;
 
-            using (var job = JobTracker.StartAsync("adamo", getConnection).Result)
+            using (var job = JobTracker.StartAsync("adamo", GetConnection).Result)
             {
+                jobId = job.JobId;
                 job.FailedAsync(new Exception("this is an error")).Wait();
+            }
+
+            AssertJobExists(jobId);
+
+            using (var cn = GetConnection())
+            {
+                Assert.IsTrue(cn.ExistsWhereAsync<Error>(new { jobId }).Result);
+            }
+        }
+
+        [TestMethod]
+        public void ExplicitSucceedJob()
+        {
+            long jobId = 0;
+
+            using (var job = JobTracker.StartAsync("adamo", GetConnection).Result)
+            {
+                jobId = job.JobId;
+                job.SucceededAsync().Wait();
+            }
+
+            AssertJobExists(jobId);
+        }
+
+        private static void AssertJobExists(long jobId)
+        {
+            using (var cn = GetConnection())
+            {
+                Assert.IsTrue(cn.ExistsAsync<Job>(jobId).Result);
             }
         }
     }
